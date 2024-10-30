d48b29bd9f7a32c42181694ad9594c0aa24e5b02
Tucker LoCicero <t.g.locicero@gmail.com>
2024-10-30 09:37:24 -0400

    init

--- homemade.py
+++ homemade.py
@@ -3,18 +3,122 @@ Some example classes for people who want to create a homemade bot.
 
 With these classes, bot makers will not have to implement the UCI or XBoard interfaces themselves.
 """
+import time
+
 import chess
+from chess import polyglot
 from chess.engine import PlayResult, Limit
 import random
 from lib.engine_wrapper import MinimalEngine
 from lib.types import MOVE, HOMEMADE_ARGS_TYPE
 import logging
 
+from timeout import Timeout
 
 # Use this logger variable to print messages to the console or log files.
 # logger.info("message") will always print "message" to the console or log file.
 # logger.debug("message") will only print "message" if verbose logging is enabled.
 logger = logging.getLogger(__name__)
+VALUE_RANKED_PIECES = [chess.QUEEN, chess.ROOK, chess.BISHOP, chess.KNIGHT, chess.PAWN]
+MAX_MOVE_TIME = 90
+
+
+class GigZordEngine(MinimalEngine):
+    transposition_table = {}
+
+    def eval_board(self, board: chess.Board) -> float:
+        zobrist_key = polyglot.zobrist_hash(board)
+        memoized_eval = self.transposition_table.get(zobrist_key)
+        if memoized_eval is not None:
+            return memoized_eval * (1 if board.turn else -1)
+
+        num_white_kings = len(board.pieces(chess.KING, chess.WHITE))
+        num_white_queens = len(board.pieces(chess.QUEEN, chess.WHITE))
+        num_white_rooks = len(board.pieces(chess.ROOK, chess.WHITE))
+        num_white_bishops_and_knights = len(board.pieces(chess.BISHOP, chess.WHITE)) + len(board.pieces(chess.KNIGHT, chess.WHITE))
+        num_white_pawns = len(board.pieces(chess.PAWN, chess.WHITE))
+
+        num_black_kings = len(board.pieces(chess.KING, chess.BLACK))
+        num_black_queens = len(board.pieces(chess.QUEEN, chess.BLACK))
+        num_black_rooks = len(board.pieces(chess.ROOK, chess.BLACK))
+        num_black_bishops_and_knights = len(board.pieces(chess.BISHOP, chess.BLACK)) + len(board.pieces(chess.KNIGHT, chess.BLACK))
+        num_black_pawns = len(board.pieces(chess.PAWN, chess.BLACK))
+
+        material_score = (
+                200 * (num_white_kings - num_black_kings)
+                + 9 * (num_white_queens - num_black_queens)
+                + 5 * (num_white_rooks - num_black_rooks)
+                + 3 * (num_white_bishops_and_knights - num_black_bishops_and_knights)
+                + 1 * (num_white_pawns - num_black_pawns)
+        )
+
+        is_white_turn = board.turn
+        board.turn = True
+        white_mobility = board.legal_moves.count()
+        board.turn = False
+        black_mobility = board.legal_moves.count()
+        board.turn = is_white_turn
+        mobility_score = 0.1 * (white_mobility - black_mobility)
+
+        evaluation = material_score + mobility_score
+        self.transposition_table[zobrist_key] = evaluation
+        return (material_score + mobility_score) * (1 if board.turn else -1)
+
+    def ordered_moves(self, board):
+        # https://www.chessprogramming.org/MVV-LVA
+        moves = board.legal_moves
+        if self.best_move_found and moves and self.best_move_found in moves:
+            moves = list(board.legal_moves)
+            moves.remove(self.best_move_found)
+            moves = [self.best_move_found] + moves
+        return moves
+
+    def negamax(self, board: chess.Board, depth: int, alpha: float, beta: float, prev_move=None) -> (chess.Move, int):
+        if board.is_checkmate():
+            return prev_move, float('inf') if board.turn else float('-inf')
+
+        if 0 in [depth, board.legal_moves.count()]:
+            return prev_move, self.eval_board(board)
+
+        best_score = float('-inf')
+        best_move = None
+        for move in self.ordered_moves(board):
+            board.push(move)
+            score = -1 * self.negamax(board, depth - 1, -1 * beta, -1 * alpha, move)[1]
+            board.pop()
+            if score > best_score:
+                best_score = score
+                best_move = move
+            alpha = max(alpha, best_score)
+            if beta <= alpha:
+                break
+        return best_move, best_score
+
+    def search(self, board: chess.Board, *args: HOMEMADE_ARGS_TYPE) -> PlayResult:
+        start_time = time.time()
+        depth = 1
+        max_depth = 5
+
+        # self.best_move_found = None
+        move, score = self.negamax(board, depth, float('-inf'), float('inf'))
+        logger.info(f" - depth {depth}: {move}, {score}")
+        with Timeout(MAX_MOVE_TIME):
+            try:
+                while depth < max_depth:
+                    depth += 1
+                    move, score = self.negamax(board, depth, float('-inf'), float('inf'))
+                    self.best_move_found = move
+                    logger.info(f" - depth {depth}: {move}, {score}")
+                    if time.time() - start_time < MAX_MOVE_TIME / 100 and depth > 3:
+                        max_depth += 1
+            except TimeoutError:
+                depth -= 1
+
+        logger.info(f"Best Move: {move}")
+        logger.info(f"Time: {round(time.time() - start_time, 2)} seconds")
+        logger.info(f"Depth: {depth}")
+        logger.info(f"Eval: {score}")
+        return PlayResult(move, ponder=None)
 
 
 class ExampleEngine(MinimalEngine):
